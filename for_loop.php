5346a20ae6804159c9e6a99a020eaff55cc5fc26
mili akter <aktermili75@gmail.com>
2023-09-17 10:30:07 +0600

    first commit

---
+++ for_loop.php
@@ -0,0 +1,13 @@
+<?php 
+for($i=1; $i<=10; $i++){
+    echo "Time table of : $i \n";
+    for($j=1; $j<=10; $j++){
+        $product=$i*$j;
+        echo "$i x $j=$product\n";
+    }
+    echo "\n";
+}
+
+
+
+?>
---
+++ loop.php
@@ -0,0 +1,14 @@
+<?php 
+
+$x=5;
+$y=2;
+echo $x . PHP_EOL;
+echo ++$x . PHP_EOL;
+echo $x . PHP_EOL;
+echo $y . PHP_EOL;
+echo $y++ . PHP_EOL;
+echo $y;
+
+
+?>
+// PHP_EOL DIA LEKA NICHA NICHA ASE;
